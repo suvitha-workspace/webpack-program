380deef474f6f653be1c3191ee9906bcde2df554
suvitha-workspace <80394034+suvitha-workspace@users.noreply.github.com>
2021-03-10 19:25:27 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css'
+class App extends Component{
+   render(){
+      return(
+         <div>
+            <h1>Welcome to Webpack Program</h1>
+         </div>
+      );
+   }
+}
+export default App;
