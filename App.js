300fbbeb96ce07f1aecff5158a7aa2ce37fb71c3
suvitha-workspace <80394034+suvitha-workspace@users.noreply.github.com>
2021-03-10 19:24:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Home from './src/Home'
+class App extends Component{
+   render(){
+      return(
+        <Home></Home>
+      );
+   }
+}
+export default App;
