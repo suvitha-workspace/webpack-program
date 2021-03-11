6d0927d91f51fec99aafb880804249c62c0844b7
suvitha-workspace <80394034+suvitha-workspace@users.noreply.github.com>
2021-03-11 17:57:09 +0530

    Update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,18 +13,17 @@ module.exports = merge(commonConfig, {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
+        use: [          
+          'style-loader',          
+          'css-loader',         
           'sass-loader',
         ],
+        exclude: /node_modules/,
       },
     ]
   }
